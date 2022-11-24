6928ffecdafa2c7568f8b15c7c4a8e5cca1c0445
Daniela Paz <danielalizama.a@gmail.com>
2022-11-24 17:54:03 -0300

    prueba

---
+++ imports/ui/App.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { useTracker } from 'meteor/react-meteor-data';
+import { TasksCollection } from '/imports/api/TasksCollection';
+import { Task } from './Task';
+import { RegisterForm } from './components/RegisterForm';
+
+const toggleChecked = ({ _id, isChecked }) => {
+  TasksCollection.update(_id, {
+    $set: {
+      isChecked: !isChecked
+    }
+  })
+};
+
+const deleteTask = ({ _id }) => TasksCollection.remove(_id);
+
+export const App = () => {
+  const tasks = useTracker(() => TasksCollection.find({}, { sort: { createdAt: -1 } }).fetch());
+
+  return (
+    <div>
+      <h1>Welcome to Meteor!</h1>
+      <RegisterForm />
+      <ul>
+        { tasks.map(task => <Task key={ task._id } task={ task } onCheckboxClick={toggleChecked} onDeleteClick={deleteTask}/>) }
+      </ul>
+    </div>
+  );
+};
